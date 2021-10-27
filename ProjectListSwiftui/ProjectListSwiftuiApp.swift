cd4b3ec275fc3375bebf77010d3a9a8aea79990a
lorena <lorenaggama@gmail.com>
2021-10-27 17:18:06 -0300

    Initial Commit

---
+++ ProjectListSwiftui/ProjectListSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectListSwiftuiApp.swift
+//  ProjectListSwiftui
+//
+//  Created by Lorena Gama on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectListSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
